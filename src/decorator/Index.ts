6c3b9d6397b268646a8d9558780d259c1bc5be28
Avijitbera <avijitbera775@gmail.com>
2025-01-16 20:32:25 +0530

    added relationship properties

---
+++ src/decorator/Index.ts
@@ -0,0 +1,4 @@
+export * as Node from './Node'
+export * as Relationship from './Relationship'
+export * as Property from './Property'
+
--- src/decorator/Relationship.ts
+++ src/decorator/Relationship.ts
@@ -1,10 +1,13 @@
 import 'reflect-metadata';
 export const RELATIONSHIP_METADATA_KEY = 'neo-type:relationship';
 
-export function Relationship(type: string, direction: 'in' | 'out' = 'out') {
+export function Relationship(type: string,
+     direction: 'in' | 'out' = 'out',
+    options: {properties?: any}
+    ) {
   return (target: any, key: string) => {
     const relationships = Reflect.getMetadata(RELATIONSHIP_METADATA_KEY, target) || [];
-    relationships.push({ key, type, direction });
+    relationships.push({ key, type, direction, properties:options.properties });
     Reflect.defineMetadata(RELATIONSHIP_METADATA_KEY, relationships, target);
   };
 }
